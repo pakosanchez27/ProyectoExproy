c2e74bdc68e2ef8a72d546b49eea8a15bb84a870
Francisco Javier Sanchez <javier.sanchezjps27@gmail.com>
2023-06-05 10:33:39 -0600

    update: Formulario de candidato sin funcionalidad ni validacion

--- js/app.js
+++ js/app.js
@@ -2,16 +2,19 @@
 const menu = document.querySelector('#menu');
 const cerrar = document.querySelector('#cerrar');
 const menuMobile = document.querySelector('#menuMobile');
+const subirFoto = document.querySelector('#inputFile');
 
 // eventos
 eventListeners();
+
 function eventListeners() {
     addEventListener('DOMContentLoaded', () => {
-        console.log('Hola mundo');
+        menu.addEventListener('click', mostrarMenu);
+        cerrar.addEventListener('click', cerrarMenu);
+        subirFoto.addEventListener('change', mostrarImagen);
     });
 
-    menu.addEventListener('click', mostrarMenu);
-    cerrar.addEventListener('click', cerrarMenu);
+  
 
 }
 
@@ -32,3 +35,19 @@ function cerrarMenu(e) {
         menuMobile.classList.add('ocultarMenu');
     }
 }
+
+// funcion para mostrar la imagen en formulario
+function mostrarImagen(event) {
+    var input = event.target;
+    console.log(input);
+    var reader = new FileReader();
+  
+    reader.onload = function() {
+      var dataURL = reader.result;
+      var imagenPreview = document.querySelector(".previewPerfil__foto");
+  
+      imagenPreview.style.backgroundImage = "url('" + dataURL + "')";
+    };
+  
+    reader.readAsDataURL(input.files[0]);
+  }
