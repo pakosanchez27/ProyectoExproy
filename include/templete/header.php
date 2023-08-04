9dda1a78c8c714fc5a7ecd24e4b1a7bdb9336155
Francisco Javier Sanchez <javier.sanchezjps27@gmail.com>
2023-08-04 01:35:46 -0600

    update

--- include/templete/header.php
+++ include/templete/header.php
@@ -141,7 +141,7 @@
                 <h3><?php echo $nombre . " " . $apellido; ?></h3>
             </div>
 
-            <a href="CandidatoPrincipal.php" class="boton__verde">Ver perfil</a>
+            <a href="CandidatoPrincipal.php?id=<?php echo $idUsuario?>" class="boton__verde">Ver perfil</a>
             <hr>
             <nav class="perfilDesplebable__cuenta">
                 <a href="#" class="enlace">Ayuda</a>
